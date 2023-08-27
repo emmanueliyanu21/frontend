7f10344ad2bcee6ce4cee7cada885587c412ad1d
Emmanueliyanu21 <emmanuel.iyanu@kaiglo.com>
2023-08-27 09:56:24 +0100

    enhacing feature on button hover state

--- src/components/ScrollBanner/Index.tsx
+++ src/components/ScrollBanner/Index.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 
 type scrollBannerProps = {
-    data: {
-        image: string
-    }[]
-}
+  data: {
+    image: string;
+  }[];
+};
 
-const ScrollBanner = ({data}: scrollBannerProps): JSX.Element =>  {
-    const heroBanner = Array.from({ length: 5 }, () => data).flat();;
+const ScrollBanner = ({ data }: scrollBannerProps): JSX.Element => {
+  const heroBanner = Array.from({ length: 5 }, () => data).flat();
 
   return (
     <>
       <div className="hero_logos animate__animated  animate__fadeInUp">
-      {heroBanner?.map((item, index) => (
-        <img key={index} src={item?.image} alt="logos" />
+        {heroBanner?.map((item, index) => (
+          <div key={index}>
+            <img className="scroll_image" src={item?.image} alt="logos" />
+          </div>
         ))}
       </div>
     </>
   );
-}
+};
 
 export default ScrollBanner;
